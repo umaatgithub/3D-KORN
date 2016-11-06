7af04d5a5062f1603eee8101b34a39acf19244a0
Umamaheswaran Raman Kumar <umamahesh.research@gmail.com>
2016-11-06 21:29:00 +0100

    Loaded pointcloud in the GUI using qvtk widget

--- Source-Code/3D-KORN/tdk_centralwidget.h
+++ Source-Code/3D-KORN/tdk_centralwidget.h
@@ -7,6 +7,17 @@
 #include <QScrollArea>
 #include <QLabel>
 #include <QPushButton>
+#include <QVTKWidget.h>
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+
+#include <pcl/visualization/pcl_visualizer.h>
+#include <vtkRenderWindow.h>
+
+typedef pcl::PointXYZRGBA PointT;
+typedef pcl::PointCloud<PointT> PointCloudT;
 
 class TDK_CentralWidget : public QWidget
 {
@@ -15,7 +26,15 @@ public:
     explicit TDK_CentralWidget(QWidget *parent = 0);
 
 private:
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
+    PointCloudT::Ptr cloud;
+    /** @brief 0 = x | 1 = y | 2 = z */
+    int filtering_axis;
+
+    /** @brief Holds the color mode for @ref colorCloudDistances */
+    int color_mode;
     QGridLayout *layout;
+
 
 signals:
 
