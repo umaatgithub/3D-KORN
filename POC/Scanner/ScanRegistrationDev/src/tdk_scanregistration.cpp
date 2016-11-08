825dc7fd7a2a55ba2f014385035240ba20cd9fca
ezequieldlrosa <ezequieldlrosa@gmail.com>
2016-11-08 21:41:43 +0100

    Add Normal Estimation to ScanRegistration

--- POC/Scanner/ScanRegistrationDev/src/tdk_scanregistration.cpp
+++ POC/Scanner/ScanRegistrationDev/src/tdk_scanregistration.cpp
@@ -11,6 +11,7 @@ TDK_ScanRegistration::TDK_ScanRegistration()
     qDebug() << "Has creado una instancia de TDK_ScanRegistration" << endl;
 
     mv_voxelSideLength = 0.02;
+    mv_FeatureRadiusSearch=0.05;
 }
 
 bool TDK_ScanRegistration::addNextPointCloud(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr &inputPointcloud)
@@ -19,13 +20,19 @@ bool TDK_ScanRegistration::addNextPointCloud(const pcl::PointCloud<pcl::PointXYZ
 
     //Downsample pointcloud and push to Downsampled vector
     mv_downSampledPointClouds.push_back(mf_voxelDownSamplePointCloud(inputPointcloud, mv_voxelSideLength));
+    mv_downSampledNormals.push_back(mf_computeNormals(*(--mv_downSampledPointClouds.end()),mv_FeatureRadiusSearch));
+    //mf_computeNormals(*(--mv_downSampledPointClouds.end()),mv_FeatureRadiusSearch);
+
+    qDebug() << "Donwsampled Point Cloud Dimension is " << (--mv_downSampledPointClouds.end())->get()->points.size()  <<endl;
+    qDebug() << "Normals Donwsampled Point Cloud Dimension is " << (--mv_downSampledNormals.end())->get()->points.size()  <<endl;
+
 
     return true;
 }
 
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr TDK_ScanRegistration::getLastOriginalPointcloud()
 {
-    return *(--mv_originalPointClouds.end());
+    return *(--mv_downSampledPointClouds.end());
 }
 
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr TDK_ScanRegistration::mf_voxelDownSamplePointCloud(const PointCloudT::Ptr &cloud_in, const float &voxelSideLength)
@@ -39,4 +46,15 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr TDK_ScanRegistration::mf_voxelDownSampleP
     vg.filter (*downSampledPointCloud);
 
     return downSampledPointCloud;
+}
+
+pcl::PointCloud<pcl::Normal>::Ptr TDK_ScanRegistration::mf_computeNormals(const PointCloudT::Ptr &cloud_in, const float &searchRadius)
+{
+    SurfaceNormalsT::Ptr normals(new SurfaceNormalsT);
+    pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> norm_est;
+    norm_est.setInputCloud (cloud_in);
+    //norm_est.setSearchMethod (new pcl::search::KdTree<pcl::PointXYZRGB>);
+    norm_est.setRadiusSearch (searchRadius);
+    norm_est.compute (*normals);
+    return normals;
 }
--- POC/Scanner/ScanRegistrationDev/src/tdk_scanregistration.h
+++ POC/Scanner/ScanRegistrationDev/src/tdk_scanregistration.h
@@ -5,6 +5,8 @@
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree_flann.h>
 
 using namespace std;
 
@@ -18,16 +20,20 @@ public:
     PointCloudT::Ptr getLastOriginalPointcloud();
 
 private:
+    typedef pcl::PointCloud<pcl::Normal> SurfaceNormalsT;
     //Configuration parameters
     float mv_voxelSideLength;
+    float mv_FeatureRadiusSearch;
+
 
     //Member variables
     vector<PointCloudT::Ptr> mv_originalPointClouds;
     vector<PointCloudT::Ptr> mv_downSampledPointClouds;
+    vector<SurfaceNormalsT::Ptr> mv_downSampledNormals;
 
     //Private Member functions
     PointCloudT::Ptr mf_voxelDownSamplePointCloud(const PointCloudT::Ptr &cloud_in, const float &voxelSideLength);
-
+    SurfaceNormalsT::Ptr mf_computeNormals(const PointCloudT::Ptr &cloud_in, const float &searchRadius);
 };
 
 #endif // TDK_SCANREGISTRATION_H
