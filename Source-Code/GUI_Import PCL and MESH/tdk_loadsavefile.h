088785eabe9ef5f319da2d92413c3b506499e10b
ctmengdi <mengdi_momo@sina.cn>
2016-12-30 21:33:33 +0100

    TDK_LoadSaveFile class optimized a bit

--- Source-Code/GUI_Import PCL and MESH/tdk_loadsavefile.h
+++ Source-Code/GUI_Import PCL and MESH/tdk_loadsavefile.h
@@ -6,20 +6,20 @@
 
 class TDK_LoadSaveFile
 {
-    pcl::PointCloud<pcl::PointXYZ> *load_cloud;
-    pcl::PointCloud<pcl::PointXYZ> *save_cloud;
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
-    pcl::PolygonMesh::Ptr *mesh;
 
 
 public:
     TDK_LoadSaveFile();
-    static void VTK2STL(const char *, const char *);
-    static void LoadPCD(const char *, pcl::PointCloud<pcl::PointXYZ>::Ptr &  );
-    static void SavePCD(const char *, pcl::PointCloud<pcl::PointXYZ>::Ptr & );
-    static void SaveVTK(const char *, pcl::PolygonMesh::Ptr &);
-    static void LoadVTK(const char *, pcl::PolygonMesh::Ptr &);
-    static void SaveSTL(const char *, const char * , pcl::PolygonMesh::Ptr &);
+    static bool VTK2STL(const char * vtkFile, const char * stlFile);
+    static bool LoadPCD(const char * pcdFile, pcl::PointCloud<pcl::PointXYZ>::Ptr & loaded_cloud);
+    static bool LoadPLY(const char * plyFile, pcl::PointCloud<pcl::PointXYZ>::Ptr & loaded_cloud );
+    static bool SavePCD(const char * pcdFile, pcl::PointCloud<pcl::PointXYZ>::Ptr & saved_cloud);
+    static bool SaveVTK(const char * vtkFile, pcl::PolygonMesh::Ptr & mesh);
+    static bool LoadVTK(const char * vtkFile, pcl::PolygonMesh::Ptr & mesh);
+    static bool SaveSTL(const char * stlFile, const char * vtkFile, pcl::PolygonMesh::Ptr & mesh);
+
+    // The "..File" is the path ended by corresponding file type, for example : vtkFile = "../xxx.vtk"
+
 
 };
 
